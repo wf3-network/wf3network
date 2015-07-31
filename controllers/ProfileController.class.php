fb34d2b23df7da5e3a9a1fa1be5cc9dc60652bea
alyrodriguez <aly.rodrgz@gmail.com>
2015-07-31 13:05:03 +0200

    Update profile

--- controllers/ProfileController.class.php
+++ controllers/ProfileController.class.php
@@ -3,34 +3,32 @@
 class ProfileController extends BaseController {
 
 	public function view() {
-		
-		$params = $this->getParams();
 
+		$params = $this->getParams();
 		if (empty($params[0])) {
 			throw new ActionControllerException('Undefined post id');
 		}
 
 		$id = (int) $params[0];
 
-
 		$profile = Profile::get($id);
 
-		$formations = Profile::getList('SELECT * FROM profile_formation INNER JOIN profile ON profile_formation.profile_id =  profile.id ORDER BY date_formation DESC');
+		$formations = Profile::getList('SELECT * FROM profile p LEFT JOIN profile_formation pf ON pf.profile_id = p.id WHERE pf.profile_id = :id ORDER BY pf.date_formation DESC', array('id' => $id));
 
-		$jobs = Profile::getList('SELECT * FROM profile INNER JOIN profile_expro ON profile.id = profile_expro.profile_id ORDER BY date_job DESC');
+		$jobs = Profile::getList('SELECT * FROM profile p LEFT JOIN profile_expro pe ON p.id = pe.profile_id WHERE pe.profile_id = :id ORDER BY pe.date_job DESC', array('id' => $id));
 		/*  */
 		$vars = array(
 			'profile' => $profile,
 			'formations' => $formations,
 			'jobs' => $jobs
 		);
-		
-		// if(!(User::isLogged)){
-		// 	$this->render('profile-simple.tpl', $vars);
-		// }else{
-		// 	$this->render('profile-full.tpl', $vars);
-		// }
-		$this->render('profile-full.tpl', $vars);
+
+		// Gère la vue sur les profils: si loggé voit profil complet sinon profil simple
+		if (User::isLogged()) {
+			$this->render('profile-full.tpl', $vars);
+		}else{
+			$this->render('profile-simple.tpl', $vars);
+		}
 	}
 
 }
