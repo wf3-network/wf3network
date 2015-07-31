7361880dcabce676c3f1253596d4d1a7db57c5f1
HkGiant <cyr.slsn@gmail.com>
2015-07-31 17:18:42 +0200

    Update navbar loggin name

--- controllers/HomeController.class.php
+++ controllers/HomeController.class.php
@@ -4,7 +4,21 @@ class HomeController extends BaseController {
 
 	public function index() {
 
-    $this->render('index');
+  	$page = (int) $this->getParam(0, 1);
+
+	$rand_profiles = Profile::getList('SELECT * FROM profile ORDER BY RAND() LIMIT 3');
+
+	// $profile = Profile::getList('SELECT * FROM profile p LEFT JOIN profile_formation pf ON pf.profile_id = p.id WHERE pf.profile_id = :id ORDER BY pf.date_formation DESC', array('id' => $id));
+
+	$profile = Profile::getList('SELECT * FROM user u LEFT JOIN profile p ON p.id = u.profile_id');
+
+	$vars = array(
+			'profile' => $profile,
+			'rand_profiles' => $rand_profiles,
+			'page' => $page,
+		);
+
+		$this->render('index', $vars);
 
 	}
 
