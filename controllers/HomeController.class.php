8c4340a26895c7cee53b978f9502a5cbdb516437
alyrodriguez <aly.rodrgz@gmail.com>
2015-08-04 17:55:47 +0200

    changes for cvform

--- controllers/HomeController.class.php
+++ controllers/HomeController.class.php
@@ -4,16 +4,13 @@ class HomeController extends BaseController {
 
 	public function index() {
 
-  	$page = (int) $this->getParam(0, 1);
+	  	$page = (int) $this->getParam(0, 1);
 
-	$rand_profiles = Profile::getList('SELECT * FROM profile ORDER BY RAND() LIMIT 3');
+		$rand_profiles = Profile::getList('SELECT * FROM profile ORDER BY RAND() LIMIT 3');
 
-	// $profile = Profile::getList('SELECT * FROM profile p LEFT JOIN profile_formation pf ON pf.profile_id = p.id WHERE pf.profile_id = :id ORDER BY pf.date_formation DESC', array('id' => $id));
+		//$profiles = Profile::getList('SELECT p.* FROM profile p, user u WHERE u.id = p.user_id');
 
-	$profile = Profile::getList('SELECT * FROM user u LEFT JOIN profile p ON p.id = u.profile_id');
-
-	$vars = array(
-			'profile' => $profile,
+		$vars = array(
 			'rand_profiles' => $rand_profiles,
 			'page' => $page,
 		);
